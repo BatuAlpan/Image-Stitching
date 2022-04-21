799ab30d2fb400506c842ce039454d651dcab2fd
Batu Alpan <batualpanfb@gmail.com>
2022-04-21 07:53:50 +0300

    cvtColor loop

--- stitcherMain.py
+++ stitcherMain.py
@@ -14,17 +14,15 @@ keypointThr = 100
 distanceThr = 250
 
 image1 = cv.imread(r"example6\example6-1.jpeg")
-image1 = cv.cvtColor(image1, cv.COLOR_BGR2RGB)
 image2 = cv.imread(r"example6\example6-2.jpeg")
-image2 = cv.cvtColor(image2, cv.COLOR_BGR2RGB)
 image3 = cv.imread(r"example6\example6-3.jpeg")
-image3 = cv.cvtColor(image3, cv.COLOR_BGR2RGB)
 image4 = cv.imread(r"example6\example6-4.jpeg")
-image4 = cv.cvtColor(image4, cv.COLOR_BGR2RGB)
 image5 = cv.imread(r"example6\example6-5.jpeg")
-image5 = cv.cvtColor(image5, cv.COLOR_BGR2RGB)
 
 images = [image1, image2, image3, image4, image5]
+
+for image in images:
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
 stitcher = sis()
 output = stitcher.stitchImages(images,ransacTol,keypointThr,distanceThr)
